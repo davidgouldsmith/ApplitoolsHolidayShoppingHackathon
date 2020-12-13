d43918acaf8fd065b8ee079324b7dd7eb7af13b6
David Gouldsmith <david.gouldsmith@willowtreeapps.com>
2020-12-13 00:49:33 -0500

    pushing project up to github

---
+++ applitools.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apiKey: 'YOUR_API_KEY',
+    batchName: 'Holiday Shopping',
+    appName: 'AppliFashion',
+    browser: [
+        {width: 1200, height: 800, name: 'chrome'},
+        {width: 1200, height: 800, name: 'firefox'},
+        {width: 1200, height: 800, name: 'edgechromium'},
+        {width: 1200, height: 800, name: 'safari'},
+        {deviceName: 'iPhone X'}
+    ]
+  }
---
+++ cypress/integration/mainPage.spec.js
@@ -0,0 +1,40 @@
+  /// <reference types="@applitools/eyes-cypress" />
+
+describe("AppliFashion Main Page", () => {
+  beforeEach(() => {
+    cy.visit('https://demo.applitools.com/tlcHackathonMasterV2.html');
+  });
+  afterEach(() => {
+    cy.eyesClose();
+  });
+  it("Test 1", () => {
+    cy.eyesOpen({
+      appName: "AppliFashion",
+      testName: "Test 1",
+      stepName: "main page",
+    });
+    cy.eyesCheckWindow("Main Page");
+  });
+  it("Test 2", () => {
+    cy.get("#LABEL__containerc__104").click();
+    cy.get("#filterBtn").click();
+    cy.eyesOpen({
+      appName: "AppliFashion",
+      testName: "Test 2",
+      stepName: "filter by color",
+    });
+    cy.eyesCheckWindow({
+        target: 'region',
+        selector: '#product_grid'
+    })
+  });
+  it("Test 3", () => {
+    cy.get("#IMG__imgfluid__215").click();
+    cy.eyesOpen({
+      appName: "AppliFashion",
+      testName: "Test 3",
+      stepName: "product details",
+    });
+    cy.eyesCheckWindow()
+  });
+});
